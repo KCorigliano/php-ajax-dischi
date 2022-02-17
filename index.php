7c0b8973c31af905a620e3f18b2690f65b4528d7
KCorigliano <corigliano98@hotmail.com>
2022-02-17 14:20:32 +0100

    init

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+
+$db=[
+    [
+        'title' => 'new jersey',
+        'img' => '',
+        'author' => 'Bon Jovi',
+        'year' => '1988',
+    ],
+    [
+        'title' => 'new jersey',
+        'img' => '',
+        'author' => 'Bon Jovi',
+        'year' => '1988',
+    ],
+    [
+        'title' => 'new jersey',
+        'img' => '',
+        'author' => 'Bon Jovi',
+        'year' => '1988',
+    ],
+    [
+        'title' => 'new jersey',
+        'img' => '',
+        'author' => 'Bon Jovi',
+        'year' => '1988',
+    ],
+    [
+        'title' => 'new jersey',
+        'img' => '',
+        'author' => 'Bon Jovi',
+        'year' => '1988',
+    ],
+    [
+        'title' => 'new jersey',
+        'img' => '',
+        'author' => 'Bon Jovi',
+        'year' => '1988',
+    ],
+    [
+        'title' => 'new jersey',
+        'img' => '',
+        'author' => 'Bon Jovi',
+        'year' => '1988',
+    ],
+    [
+        'title' => 'new jersey',
+        'img' => '',
+        'author' => 'Bon Jovi',
+        'year' => '1988',
+    ],
+    [
+        'title' => 'new jersey',
+        'img' => '',
+        'author' => 'Bon Jovi',
+        'year' => '1988',
+    ],
+    [
+        'title' => 'new jersey',
+        'img' => '',
+        'author' => 'Bon Jovi',
+        'year' => '1988',
+    ],
+    [
+        'title' => 'new jersey',
+        'img' => '',
+        'author' => 'Bon Jovi',
+        'year' => '1988',
+    ],
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
