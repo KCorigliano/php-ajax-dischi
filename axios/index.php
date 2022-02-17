a0ae0f3b11efbb20480cdbf1dd50235bb1c8a8fd
KCorigliano <corigliano98@hotmail.com>
2022-02-17 15:48:58 +0100

    vue init

--- axios/index.php
+++ axios/index.php
@@ -63,36 +63,8 @@ $db=[
     ],
 ];
 
+header('Content-Type: application/json');
+
+echo json_encode($db);
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-</head>
-<body>
-
-<header></header>
-
-<div class="container">
-    <?php
-        foreach ($db as $disc) {
-            echo 
-            "<div class=\"disc-container\">
-                <div class=\"img-container\"><img src=" . $disc["img"] . " alt=\"\"></div>
-                <h4>" . $disc["title"] . "</h4>
-                <p>" . $disc["author"] . "</p>
-                <p>" . $disc["year"] . "</p>
-            </div>";
-        }
-    ?>
-</div>
-
-
-</body>
-</html>
