1979d1b7896c97bb6db7a6396caa46f1c3126d1c
KCorigliano <corigliano98@hotmail.com>
2022-02-17 16:08:24 +0100

    vue version

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-    }
+        discs: '',
+    },
+    mounted() {
+        axios.get('http://localhost/php-ajax-dischi/axios/disc.php').then((result) => {
+            console.log(result.data);
+            this.discs=result.data;
+        })
+    },
   })
--- vue.php
+++ vue.php
@@ -6,17 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 </head>
 <body>
 
     <div id="app">
         <header></header>
         <div class="container">
-            <div class="disc-container">
-                <div class="img-container"><img src="" alt=""></div>
-                <h4></h4>
-                <p></p>
-                <p></p>
+            <div class="disc-container" v-for="disc in discs">
+                <div class="img-container"><img :src="disc.img" alt=""></div>
+                <h4>{{disc.title}}</h4>
+                <p>{{disc.author}}</p>
+                <p>{{disc.year}}</p>
             </div>
         </div>
     </div>
